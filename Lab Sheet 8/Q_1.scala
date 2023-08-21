a909c4b2aa48bf99d90201cb6fb1e7155f18731c
N N Ariyawansa <112557186+NusharaAriyawansa@users.noreply.github.com>
2023-08-22 01:11:31 +0530

    Add files via upload

---
+++ Lab Sheet 8/Q_1.scala
@@ -0,0 +1,18 @@
+object Q_1 {
+    val calculateInterest = (deposit: Double) => {
+        if (deposit <= 20000) deposit*0.02
+        else if (deposit <= 200000) deposit*0.04
+        else if (deposit <= 2000000) deposit*0.035
+        else deposit*0.065
+    }    
+
+    def main(args: Array[String]): Unit = {
+        var amount = 30000; 
+        var interest = calculateInterest(amount); 
+        println("Interest = " + "%.2f".format(interest));
+    }
+}
+
+
+// val lambda_exp = (variable:Type) => Transformation_Expression
+
